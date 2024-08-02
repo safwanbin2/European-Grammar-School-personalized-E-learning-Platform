2f4a4e72901cdebbce1ede10463234ad9874d718
safwanbin2 <safwanridwan321@gmail.com>
2024-08-02 18:28:24 +0600

    footer added in home

--- src/pages/dashboard/home/DashboardHome.jsx
+++ src/pages/dashboard/home/DashboardHome.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import homeBanner from "../../../assets/homeBanner.png";
+import Footer from "../../shared/footer/Footer";
 
 const DashboardHome = () => {
   return (
     <div>
       <div>
-        <h2 className="text-base font-semibold">Welcome to</h2>
-        <p className="text-xl font-semibold">
+        <h2 className="text-xl font-semibold">Welcome to</h2>
+        {/* <p className="text-xl font-semibold">
           European Grammar School personalized E-learning Platform
-        </p>
+        </p> */}
+        <img src={homeBanner} alt="" />
       </div>
+      <Footer />
     </div>
   );
 };
